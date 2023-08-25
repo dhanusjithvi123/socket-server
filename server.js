6e5d5c26a4d31e5528a5aabf4cc13087c06405c6
dhanusjithvi123 <dhanusjithvi22@gmail.com>
2023-08-25 12:29:59 +0530

    envorment set

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors'); // Import the cors package
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// Configure CORS to allow requests from http://localhost:4200
+app.use(cors({
+  origin: 'http://localhost:4200',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
+
+
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('drawing', (data) => {
+    // Broadcast drawing data to all connected clients
+    socket.broadcast.emit('drawing', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
